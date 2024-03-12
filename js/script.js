5988ed5d71706603c8bfa38d3e5f1f2463845a9f
young <makt00byoung@gmail.com>
2024-03-12 13:23:52 +0900

    헤더 바텀 > 메뉴 및 하위메뉴 스타일 적용 // java > 메뉴 눌렀을 때 서브메뉴 바탕화면에 보이기

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-window.onload = function () {};
+$(document).ready(function () {
+  // gnb 관련코드
+  var gnb = $(".mainmenu");
+  var dim = $(".header-dim");
+  gnb.mouseenter(function(){
+    dim.stop().fadeIn(200)
+  })
+  gnb.mouseleave(function(){
+    dim.stop().fadeOut(200)
+  })
+});
+// 자바스크립트 연결
+// window.onload = function () {};
